07b6d3745fdb400cdbbcf356068f48d9e636f9ca
Xdrum1 <aholmes843@outlook.com>
2026-02-24 17:40:33 -0500

    Fix projection type to use ProjectionSpecification object

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -740,7 +740,7 @@ export default function MapView({
         ref={mapRef}
         mapboxAccessToken={token}
         initialViewState={INITIAL_VIEW}
-        projection="mercator"
+        projection={{ name: "mercator" }}
         style={{ width: "100%", height: "100%" }}
         mapStyle={MAP_STYLE}
         onClick={handleMapClick}
